0df3481b22699096a44f54f0a228233ad1f87baa
Annette Jensen <annetteduedal@gmail.com>
2025-02-25 09:33:57 +0100

    Annettes mappe

---
+++ Annette/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Repetition_programmering_Annette
+{
+    internal class Program
+    {
+        int age = 25;
+        double height = 1.75;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
